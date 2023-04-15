e13ac92c845d26e5cc84f797ff6ee5dacda8fb4e
d1ordan <130851170+d1ordan@users.noreply.github.com>
2023-04-15 11:49:50 -0400

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿//Стоврити масив з 10 елементів, тобто продуктів і записати туди їх
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string food[10];
+	for (int i = 0; i < 5; i++) {
+		getline(cin, food[i]);
+	}
+
+	for (int i = 0; i < 5; i++)
+		cout << food[i] << endl;
+	
+}
